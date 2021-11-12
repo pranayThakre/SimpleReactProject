7654c7cfe22919b80cc30f26dee1f30d785bffdf
Pranay Thakre <thakrepranay47@gmail.com>
2021-11-12 16:45:55 +0530

    my comment for first commit of simple react project

---
+++ src/Components/HelloWorld.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const HelloWorld = () => {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  // function sayHello() {
+  //   alert('Hello, World! for the last time');
+  // }
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <p>This is to test React App...</p>
+      <div>
+        <div>
+          First Name:
+          <input type="text" placeholder="type First Name" onChange={e => setFirstName(e.target.value)}></input>
+        </div>
+        <br />
+        <div>
+          Last Name:
+          <input type="text" placeholder="type Last Name" onChange={e => setLastName(e.target.value)}></input>
+        </div>
+        <br />     <br />
+        Your name is <b>{firstName} {lastName}</b>
+      </div>
+      <br />
+      <button >Submit</button>
+    </div>
+  );
+};
+
+export default HelloWorld;
